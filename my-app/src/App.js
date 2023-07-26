a54f7d3baeaaba295521598f58337d4df76459f7
AHJA2002 <jaykumar.ahuja@nielseniq.com>
2023-07-26 16:07:47 +0530

    Docker file changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,78 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import LatestTimeUpdate from './LatestTimeUpdate';
+import { Header } from './components/Header'
+import { Users } from './components/Users'
+import { DisplayBoard } from './components/DisplayBoard'
+import CreateUser from './components/CreateUser'
+import { getAllUsers, createUser } from './services/UserService'
 
-function App() {
-  
+class App extends Component {
 
-  return (
-    <Router>
+  state = {
+    user: {},
+    users: [],
+    numberOfUsers: 0
+  }
+
+  createUser = (e) => {
+      createUser(this.state.user)
+        .then(response => {
+          console.log(response);
+          this.setState({numberOfUsers: this.state.numberOfUsers + 1})
+      });
+  }
+
+  getAllUsers = () => {
+    getAllUsers()
+      .then(users => {
+        console.log(users)
+        this.setState({users: users, numberOfUsers: users.length})
+      });
+  }
+
+  onChangeForm = (e) => {
+      let user = this.state.user
+      if (e.target.name === 'firstname') {
+          user.firstName = e.target.value;
+      } else if (e.target.name === 'lastname') {
+          user.lastName = e.target.value;
+      } else if (e.target.name === 'email') {
+          user.email = e.target.value;
+      }
+      this.setState({user})
+  }
+
+  render() {
+    
+    return (
       <div className="App">
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/sample">Another page</Link></li>
-          </ul>
-        </nav>
-      <LatestTimeUpdate/>
+        <Header></Header>
+        <div className="container mrgnbtm">
+          <div className="row">
+            <div className="col-md-8">
+                <CreateUser 
+                  user={this.state.user}
+                  onChangeForm={this.onChangeForm}
+                  createUser={this.createUser}
+                  >
+                </CreateUser>
+            </div>
+            <div className="col-md-4">
+                <DisplayBoard
+                  numberOfUsers={this.state.numberOfUsers}
+                  getAllUsers={this.getAllUsers}
+                >
+                </DisplayBoard>
+            </div>
+          </div>
+        </div>
+        <div className="row mrgnbtm">
+          <Users users={this.state.users}></Users>
+        </div>
       </div>
-    </Router>
-  );
+    );
+  }
 }
-
-const Home = props => (<div>Home</div>);
-const AnotherPage = props => (<div>Another page</div>)
 
 export default App;
